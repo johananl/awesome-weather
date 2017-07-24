9fed0b756ad73eddcc16ef71466c7dd6f7dce93f
Johanan Liebermann <johanan.lieberman@gmail.com>
2017-07-24 23:49:28 +0300

    Add search action

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,7 +1,8 @@
 class WeatherController < ApplicationController
-  def index
+  def search
     openweathermap_api_key = Rails.application.secrets.openweathermap_api_key
-    url = "http://api.openweathermap.org/data/2.5/weather?q=Givatayim&APPID=#{openweathermap_api_key}"
-    @data = JSON.parse(open(url).read)
+    city = params[:city]
+    url = "http://api.openweathermap.org/data/2.5/weather?q=#{city}&APPID=#{openweathermap_api_key}"
+    @weather_data = JSON.parse(open(url).read)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'weather/index'
+  get 'weather/search'
 
   root 'weather#index'
 end
