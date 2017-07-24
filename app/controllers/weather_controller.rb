71affdf2afe757fd10d377148f0f8d9564ec93e2
Johanan Liebermann <johanan.lieberman@gmail.com>
2017-07-24 23:03:49 +0300

    Test OWM API call

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,7 @@
 class WeatherController < ApplicationController
   def index
+    openweathermap_api_key = Rails.application.secrets.openweathermap_api_key
+    url = "http://api.openweathermap.org/data/2.5/weather?q=Givatayim&APPID=#{openweathermap_api_key}"
+    @data = JSON.parse(open(url).read)
   end
 end
