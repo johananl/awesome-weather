d941138ce424b9143d4999c98dcdc50dc4b136c8
Johanan Liebermann <johanan.lieberman@gmail.com>
2017-07-25 00:06:52 +0300

    Add country search param

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,8 +1,9 @@
 class WeatherController < ApplicationController
   def search
     openweathermap_api_key = Rails.application.secrets.openweathermap_api_key
+    country = params[:country]
     city = params[:city]
-    url = "http://api.openweathermap.org/data/2.5/weather?q=#{city}&APPID=#{openweathermap_api_key}"
+    url = "http://api.openweathermap.org/data/2.5/weather?q=#{city},#{country}&APPID=#{openweathermap_api_key}"
     @weather_data = JSON.parse(open(url).read)
   end
 end
