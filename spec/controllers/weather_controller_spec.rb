44d51768176f2439504e5d7a6c82a08729e2da82
Johanan Liebermann <johanan.lieberman@gmail.com>
2017-08-02 02:53:37 +0300

    Add tests for search action

--- spec/controllers/weather_controller_spec.rb
+++ spec/controllers/weather_controller_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe WeatherController, :type => :controller do
       expect(response).to render_template("index")
     end
   end
+
+  describe "GET #search" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :search, params: {:country => 'DE', :city => 'Berlin'}
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "redirects to index when the city cannot be found" do
+      get :search, params: {:country => '', :city => 'xxxxxxxxxxxxxxxx'}
+      expect(response).to redirect_to '/'
+      expect(flash[:alert]).to be_present
+    end
+  end
 end
